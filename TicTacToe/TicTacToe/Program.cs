955d90687143468509e3166cd1975215bffd64c3
justin <justin@BIGSHELL>
2016-07-18 17:21:03 -0400

    mostly works: need a delay on the win message, a way to see if there's any open spots left (array comparison?, and possibly stop player from losing turn if they pick an invalid spot

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -13,21 +13,23 @@ namespace TicTacToe
             string[,] gameBoard = new string[3, 3];
             bool gameOver = false;
             string activePlayer = "O";
+            string stringChoice = null;
 
-            while (!gameOver)
+            #region fill board
+            int counter = 1;
+            for (int i = 0; i < 3; i++)
             {
-                int counter = 1;
-
-                #region fill board
-                for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
                 {
-                    for (int j = 0; j < 3; j++)
-                    {
-                        gameBoard[i, j] = counter.ToString();
-                        counter++;
-                    }
+                    gameBoard[i, j] = counter.ToString();
+                    counter++;
                 }
-                #endregion
+            }
+            #endregion
+
+            while (gameOver == false)
+            {
+                
 
                 #region show board
                 Console.WriteLine(gameBoard[0, 0] + "|" + gameBoard[0, 1] + "|" + gameBoard[0, 2]);
@@ -38,11 +40,12 @@ namespace TicTacToe
                 Console.WriteLine();
                 #endregion
 
+                checkWinState(ref gameOver, activePlayer, gameBoard);
                 playerSwitchFunc(ref activePlayer);
-                validChoice(ref gameBoard, ref activePlayer);  //i'm unsure why this isn't working. i don't think i need to call the specific indexes here.
-
+                validChoice(ref gameBoard, activePlayer, ref stringChoice, ref gameOver);
             }
         }
+
         static void playerSwitchFunc(ref string activePlayer)
         {
             if (activePlayer == "X")
@@ -59,28 +62,174 @@ namespace TicTacToe
                 Console.ReadKey();
             }
         }
-        static void validChoice(ref string[,] gameBoard, ref string activePlayer)
+
+        static void validChoice(ref string[,] gameBoard, string activePlayer, ref string stringChoice,ref bool gameOver)
         {
             Console.WriteLine("Please choose a location: ");
             int playerChoiceFunc;
-            string stringChoice = Console.ReadLine();
+            stringChoice = Console.ReadLine();
             if (int.TryParse(stringChoice, out playerChoiceFunc))
             {
-                if (gameBoard[playerChoiceFunc] != " ")
+                switch (stringChoice)
                 {
-                    Console.WriteLine("That choice is invalid, please try again.");
-                }
-                else if ((playerChoiceFunc >= 0) && (playerChoiceFunc <= 8))
-                {
-                    gameBoard[playerChoiceFunc] = activePlayer; //this might be trying to change a string into int rather than swapping strings?
-                }
-                else
-                {
-                    Console.WriteLine("Pick a location between 0 and 8!");
-                    Console.ReadKey();
-                }
+                    case "1":
+                        if (gameBoard[0, 0] == stringChoice)
+                        {
+                            gameBoard[0, 0] = activePlayer;
+                            checkWinState(ref gameOver, activePlayer, gameBoard);
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("This is not a valid choice");
+                        }
+                        break;
+
+                    case "2":
+                        if (gameBoard[0, 1] == stringChoice)
+                        {
+                            gameBoard[0, 1] = activePlayer;
+                            checkWinState(ref gameOver, activePlayer, gameBoard);
+                        }
+                        else
+                        {
+                            Console.WriteLine("This is not a valid choice");
+                        }
+                        break;
+
+                    case "3":
+                        if (gameBoard[0, 2] == stringChoice)
+                        {
+                            gameBoard[0, 2] = activePlayer;
+                            checkWinState(ref gameOver, activePlayer, gameBoard);
+                        }
+                        else
+                        {
+                            Console.WriteLine("This is not a valid choice");
+                        }
+                        break;
+                    case "4":
+                        if (gameBoard[1, 0] == stringChoice)
+                        {
+                            gameBoard[1, 0] = activePlayer;
+                            checkWinState(ref gameOver, activePlayer, gameBoard);
+                        }
+                        else
+                        {
+                            Console.WriteLine("This is not a valid choice");
+                        }
+                        break;
+                    case "5":
+                        if (gameBoard[1, 1] == stringChoice)
+                        {
+                            gameBoard[1, 1] = activePlayer;
+                            checkWinState(ref gameOver, activePlayer, gameBoard);
+                        }
+                        else
+                        {
+                            Console.WriteLine("This is not a valid choice");
+                        }
+                        break;
+                    case "6":
+                        if (gameBoard[1, 2] == stringChoice)
+                        {
+                            gameBoard[1, 2] = activePlayer;
+                            checkWinState(ref gameOver, activePlayer, gameBoard);
+                        }
+                        else
+                        {
+                            Console.WriteLine("This is not a valid choice");
+                        }
+                        break;
+                    case "7":
+                        if (gameBoard[2, 0] == stringChoice)
+                        {
+                            gameBoard[2, 0] = activePlayer;
+                            checkWinState(ref gameOver, activePlayer, gameBoard);
+                        }
+                        else
+                        {
+                            Console.WriteLine("This is not a valid choice");
+                        }
+                        break;
+                    case "8":
+                        if (gameBoard[2, 1] == stringChoice)
+                        {
+                            gameBoard[2, 1] = activePlayer;
+                            checkWinState(ref gameOver, activePlayer, gameBoard);
+                        }
+                        else
+                        {
+                            Console.WriteLine("This is not a valid choice");
+                        }
+                        break;
+                    case "9":
+                        if (gameBoard[2, 2] == stringChoice)
+                        {
+                            gameBoard[2, 2] = activePlayer;
+                            checkWinState(ref gameOver, activePlayer, gameBoard);
+                        }
+                        else
+                        {
+                            Console.WriteLine("This is not a valid choice");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("This isn't a valid choice!");
+                        break;
                 }
             }
-
+            else
+                Console.WriteLine("Please pick 1-9");
         }
+        static bool checkWinState(ref bool gameOver, string activePlayer, string[,] gameBoard)
+        {//horizontal win conds
+            if ((gameBoard[0,0] == activePlayer) && (gameBoard[0,1] == activePlayer) && (gameBoard[0,2] == activePlayer))
+            {
+                Console.WriteLine("You win player {0}", activePlayer);
+                return gameOver = true;
+            }
+            else if ((gameBoard[1, 0] == activePlayer) && (gameBoard[1, 1] == activePlayer) && (gameBoard[1, 2] == activePlayer))
+            {
+                Console.WriteLine("You win player {0}", activePlayer);
+                return gameOver = true;
+            }
+            else if ((gameBoard[2, 0] == activePlayer) && (gameBoard[2, 1] == activePlayer) && (gameBoard[2, 2] == activePlayer))
+            {
+                Console.WriteLine("You win player {0}", activePlayer);
+                return gameOver = true;
+            }
+            //vertical win conds
+            else if ((gameBoard[0, 0] == activePlayer) && (gameBoard[1, 0] == activePlayer) && (gameBoard[2, 0] == activePlayer))
+            {
+                Console.WriteLine("You win player {0}", activePlayer);
+                return gameOver = true;
+            }
+            else if ((gameBoard[0,1] == activePlayer) && (gameBoard[1, 1] == activePlayer) && (gameBoard[2, 1] == activePlayer))
+            {
+                Console.WriteLine("You win player {0}", activePlayer);
+                return gameOver = true;
+            }
+            else if ((gameBoard[0, 2] == activePlayer) && (gameBoard[1, 2] == activePlayer) && (gameBoard[2, 2] == activePlayer))
+            {
+                Console.WriteLine("You win player {0}", activePlayer);
+                return gameOver = true;
+            }
+
+            //diag win conds
+            else if ((gameBoard[0, 0] == activePlayer) && (gameBoard[1, 1] == activePlayer) && (gameBoard[2, 2] == activePlayer))
+            {
+                Console.WriteLine("You win player {0}", activePlayer);
+                return gameOver = true;
+            }
+            else if ((gameBoard[0, 2] == activePlayer) && (gameBoard[1, 1] == activePlayer) && (gameBoard[2, 0] == activePlayer))
+            {
+                Console.WriteLine("You win player {0}", activePlayer);
+                return gameOver = true;
+            }
+            else
+                return gameOver = false;  //this should be false
+        }
+
     }
+}
